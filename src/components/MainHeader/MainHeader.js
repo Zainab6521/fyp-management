f14d74fceca13f2b6f55399a3704960ca1ff323f
Zainab Raza <zk.652162@gmail.com>
2021-06-02 00:35:31 -0700

    ...

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>FYP-Management System</h1>
+      <h1>FYP Management System</h1>
       <Navigation />
     </header>
   );
